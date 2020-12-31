4ba5e494415010d2f5792b982c277b4d404ca9a6
Rustam Mamadaminov <rmamdaminov@gmail.com>
2020-12-31 15:52:00 +0500

    Fix CS

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -25,7 +25,6 @@ use Yiisoft\Di\Container;
 use Yiisoft\Factory\Definitions\Reference;
 use Yiisoft\Log\Logger;
 use Yiisoft\Profiler\Profiler;
-
 use Yiisoft\Profiler\ProfilerInterface;
 
 use function explode;
