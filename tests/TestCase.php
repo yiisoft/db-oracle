2b5bec5d813017396b827e0362d0370883ae0019
Rustam Mamadaminov <rmamdaminov@gmail.com>
2020-12-31 16:00:30 +0500

    Fix tests for yiisoft/db#209 (#26)

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -25,6 +25,7 @@ use Yiisoft\Di\Container;
 use Yiisoft\Factory\Definitions\Reference;
 use Yiisoft\Log\Logger;
 use Yiisoft\Profiler\Profiler;
+use Yiisoft\Profiler\ProfilerInterface;
 
 use function explode;
 use function file_get_contents;
@@ -38,7 +39,7 @@ class TestCase extends AbstractTestCase
     protected ContainerInterface $container;
     protected LoggerInterface $logger;
     protected Connection $connection;
-    protected Profiler $profiler;
+    protected ProfilerInterface $profiler;
     protected array $dataProvider;
     protected QueryCache $queryCache;
     protected SchemaCache $schemaCache;
@@ -102,7 +103,7 @@ class TestCase extends AbstractTestCase
         $this->aliases = $this->container->get(Aliases::class);
         $this->cache = $this->container->get(CacheInterface::class);
         $this->logger = $this->container->get(LoggerInterface::class);
-        $this->profiler = $this->container->get(Profiler::class);
+        $this->profiler = $this->container->get(ProfilerInterface::class);
         $this->connection = $this->container->get(ConnectionInterface::class);
         $this->queryCache = $this->container->get(QueryCache::class);
         $this->schemaCache = $this->container->get(SchemaCache::class);
@@ -289,6 +290,7 @@ class TestCase extends AbstractTestCase
             ],
 
             LoggerInterface::class => Logger::class,
+            ProfilerInterface::class => Profiler::class,
 
             ConnectionInterface::class => [
                 '__class' => Connection::class,
