07060b5e153d797740a6c285abdb28d89e26fb5e
Wilmer <terabytesoftw@gmail.com>
2021-10-01 14:52:52 -0300

    Remove duplicate code.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,19 +6,23 @@ namespace Yiisoft\Db\Oracle\Tests;
 
 use PHPUnit\Framework\TestCase as AbstractTestCase;
 use Yiisoft\Db\Connection\ConnectionInterface;
-use Yiisoft\Db\Oracle\Connection;
 use Yiisoft\Db\TestUtility\TestTrait;
 
 class TestCase extends AbstractTestCase
 {
     use TestTrait;
 
-    protected const DB_DSN = 'oci:dbname=localhost/XE;charset=AL32UTF8;';
+    protected const DB_CONNECTION_CLASS = \Yiisoft\Db\Oracle\Connection::class;
+    protected const DB_DRIVERNAME = 'oci';
+    protected const DB_DSN = 'oci:dbname=localhost/XE;';
     protected const DB_FIXTURES_PATH = __DIR__ . '/Fixture/oci.sql';
+    protected const DB_USERNAME = 'system';
+    protected const DB_PASSWORD = 'oracle';
+    protected const DB_CHARSET = 'AL32UTF8';
     protected array $dataProvider;
     protected string $likeEscapeCharSql = '';
     protected array $likeParameterReplacements = [];
-    protected Connection $connection;
+    protected ConnectionInterface $connection;
 
     protected function setUp(): void
     {
@@ -38,62 +42,5 @@ class TestCase extends AbstractTestCase
             $this->schemaCache,
             $this->profiler
         );
-    }
-
-    protected function createConnection(string $dsn = null): ?ConnectionInterface
-    {
-        $db = null;
-
-        if ($dsn !== null) {
-            $db = new Connection($dsn, $this->createQueryCache(), $this->createSchemaCache());
-            $db->setLogger($this->createLogger());
-            $db->setProfiler($this->createProfiler());
-            $db->setUsername('system');
-            $db->setPassword('oracle');
-        }
-
-        return $db;
-    }
-
-    protected function prepareDatabase(string $dsn = null, $fixture = null): void
-    {
-        $fixture = $fixture ?? self::DB_FIXTURES_PATH;
-
-        if ($dsn !== null) {
-            $this->connection = $this->createConnection($dsn);
-        }
-
-        $this->connection->open();
-
-        if ($fixture !== null) {
-            [$drops, $creates] = explode('/* STATEMENTS */', file_get_contents($fixture), 2);
-
-            [$statements, $triggers, $data] = explode('/* TRIGGERS */', $creates, 3);
-
-            $lines = array_merge(
-                explode('--', $drops),
-                explode(';', $statements),
-                explode('/', $triggers),
-                explode(';', $data),
-            );
-
-            foreach ($lines as $line) {
-                if (trim($line) !== '') {
-                    $this->connection->getPDO()->exec($line);
-                }
-            }
-        }
-    }
-
-    /**
-     * Adjust dbms specific escaping.
-     *
-     * @param string $sql
-     *
-     * @return string
-     */
-    protected function replaceQuotes(string $sql): string
-    {
-        return str_replace(['[[', ']]'], '"', $sql);
     }
 }
