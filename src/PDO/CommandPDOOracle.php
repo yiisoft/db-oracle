74969325ee5c11a91d1a4d5e47cdac4514904115
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2022-03-29 11:05:36 -0300

    Fix static analisys. (#54)
    * Fix static analisys tests.

--- src/PDO/CommandPDOOracle.php
+++ src/PDO/CommandPDOOracle.php
@@ -8,7 +8,6 @@ use PDO;
 use PDOException;
 use Yiisoft\Db\Cache\QueryCache;
 use Yiisoft\Db\Command\CommandPDO;
-use Yiisoft\Db\Command\ParamInterface;
 use Yiisoft\Db\Connection\ConnectionPDOInterface;
 use Yiisoft\Db\Exception\ConvertException;
 use Yiisoft\Db\Exception\Exception;
@@ -127,10 +126,7 @@ final class CommandPDOOracle extends CommandPDO
     {
         $paramsPassedByReference = [];
 
-        /** @psalm-var ParamInterface[] */
-        $params = $this->params;
-
-        foreach ($params as $name => $value) {
+        foreach ($this->params as $name => $value) {
             if (PDO::PARAM_STR === $value->getType()) {
                 /** @var mixed */
                 $paramsPassedByReference[$name] = $value->getValue();
