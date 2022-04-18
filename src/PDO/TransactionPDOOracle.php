00c84a2caa943c3ddd0bfb3c8a166507114ed8eb
darkdef <darkdef@gmail.com>
2022-04-18 21:37:31 +0300

    styleci

--- src/PDO/TransactionPDOOracle.php
+++ src/PDO/TransactionPDOOracle.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Yiisoft\Db\Oracle\PDO;
 
-use Yiisoft\Db\Exception\NotSupportedException;
 use Yiisoft\Db\Transaction\TransactionPDO;
 
 final class TransactionPDOOracle extends TransactionPDO
