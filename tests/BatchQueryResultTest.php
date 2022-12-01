80d629c3f9c401bf6fe00c1bd46e575dcd6eb6fc
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2022-12-01 16:24:05 -0300

    Remove src\TestSupport\TestBatchQueryResultTrait::class from yiisoft\db. (#111)
    * Remove src\TestSupport\TestBatchQueryResultTrait::class from yiisoft/db.

--- tests/BatchQueryResultTest.php
+++ tests/BatchQueryResultTest.php
@@ -4,12 +4,13 @@ declare(strict_types=1);
 
 namespace Yiisoft\Db\Oracle\Tests;
 
-use Yiisoft\Db\TestSupport\TestBatchQueryResultTrait;
+use Yiisoft\Db\Oracle\Tests\Support\TestTrait;
+use Yiisoft\Db\Tests\Common\CommonBatchQueryResultTest;
 
 /**
  * @group oracle
  */
-final class BatchQueryResultTest extends TestCase
+final class BatchQueryResultTest extends CommonBatchQueryResultTest
 {
-    use TestBatchQueryResultTrait;
+    use TestTrait;
 }
