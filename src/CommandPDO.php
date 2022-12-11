0ce0422f7b6cea1aacceddc18e790b779e7fb6d6
darkdef <darkdef@gmail.com>
2022-12-11 22:23:08 +0300

    Simplify insertEx

--- src/CommandPDO.php
+++ src/CommandPDO.php
@@ -32,7 +32,7 @@ final class CommandPDO extends AbstractCommandPDO
     public function insertEx(string $table, array $columns): bool|array
     {
         $params = [];
-        $sql = $this->queryBuilder()->insertEx($table, $columns, $params);
+        $sql = $this->queryBuilder()->insert($table, $columns, $params);
 
         $tableSchema = $this->db->getSchema()->getTableSchema($table);
 
