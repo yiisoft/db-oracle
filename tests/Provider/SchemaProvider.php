c248734271eed4f135f0ca1e7c763d82b386069f
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2022-12-16 09:01:18 -0300

    Remove src\TestSupport\TestSchemaTrait from yiisoft\db. (#124)
    * Remove src\TestSupport\TestSchemaTrait from yiisoft\db.

---
+++ tests/Provider/SchemaProvider.php
@@ -0,0 +1,295 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Db\Oracle\Tests\Provider;
+
+use Yiisoft\Db\Constraint\CheckConstraint;
+use Yiisoft\Db\Tests\Provider\AbstractSchemaProvider;
+use Yiisoft\Db\Tests\Support\AnyValue;
+
+final class SchemaProvider extends AbstractSchemaProvider
+{
+    public function columns(): array
+    {
+        return [
+            [
+                [
+                    'int_col' => [
+                        'type' => 'integer',
+                        'dbType' => 'NUMBER',
+                        'phpType' => 'integer',
+                        'allowNull' => false,
+                        'autoIncrement' => false,
+                        'enumValues' => null,
+                        'size' => 22,
+                        'precision' => null,
+                        'scale' => 0,
+                        'defaultValue' => null,
+                    ],
+                    'int_col2' => [
+                        'type' => 'integer',
+                        'dbType' => 'NUMBER',
+                        'phpType' => 'integer',
+                        'allowNull' => true,
+                        'autoIncrement' => false,
+                        'enumValues' => null,
+                        'size' => 22,
+                        'precision' => null,
+                        'scale' => 0,
+                        'defaultValue' => 1,
+                    ],
+                    'tinyint_col' => [
+                        'type' => 'integer',
+                        'dbType' => 'NUMBER',
+                        'phpType' => 'integer',
+                        'allowNull' => true,
+                        'autoIncrement' => false,
+                        'enumValues' => null,
+                        'size' => 22,
+                        'precision' => 3,
+                        'scale' => 0,
+                        'defaultValue' => 1,
+                    ],
+                    'smallint_col' => [
+                        'type' => 'integer',
+                        'dbType' => 'NUMBER',
+                        'phpType' => 'integer',
+                        'allowNull' => true,
+                        'autoIncrement' => false,
+                        'enumValues' => null,
+                        'size' => 22,
+                        'precision' => null,
+                        'scale' => 0,
+                        'defaultValue' => 1,
+                    ],
+                    'char_col' => [
+                        'type' => 'string',
+                        'dbType' => 'CHAR',
+                        'phpType' => 'string',
+                        'allowNull' => false,
+                        'autoIncrement' => false,
+                        'enumValues' => null,
+                        'size' => 100,
+                        'precision' => null,
+                        'scale' => null,
+                        'defaultValue' => null,
+                    ],
+                    'char_col2' => [
+                        'type' => 'string',
+                        'dbType' => 'VARCHAR2',
+                        'phpType' => 'string',
+                        'allowNull' => true,
+                        'autoIncrement' => false,
+                        'enumValues' => null,
+                        'size' => 100,
+                        'precision' => null,
+                        'scale' => null,
+                        'defaultValue' => 'something',
+                    ],
+                    'char_col3' => [
+                        'type' => 'string',
+                        'dbType' => 'VARCHAR2',
+                        'phpType' => 'string',
+                        'allowNull' => true,
+                        'autoIncrement' => false,
+                        'enumValues' => null,
+                        'size' => 4000,
+                        'precision' => null,
+                        'scale' => null,
+                        'defaultValue' => null,
+                    ],
+                    'float_col' => [
+                        'type' => 'double',
+                        'dbType' => 'FLOAT',
+                        'phpType' => 'double',
+                        'allowNull' => false,
+                        'autoIncrement' => false,
+                        'enumValues' => null,
+                        'size' => 22,
+                        'precision' => 126,
+                        'scale' => null,
+                        'defaultValue' => null,
+                    ],
+                    'float_col2' => [
+                        'type' => 'double',
+                        'dbType' => 'FLOAT',
+                        'phpType' => 'double',
+                        'allowNull' => true,
+                        'autoIncrement' => false,
+                        'enumValues' => null,
+                        'size' => 22,
+                        'precision' => 126,
+                        'scale' => null,
+                        'defaultValue' => 1.23,
+                    ],
+                    'blob_col' => [
+                        'type' => 'binary',
+                        'dbType' => 'BLOB',
+                        'phpType' => 'resource',
+                        'allowNull' => true,
+                        'autoIncrement' => false,
+                        'enumValues' => null,
+                        'size' => 4000,
+                        'precision' => null,
+                        'scale' => null,
+                        'defaultValue' => null,
+                    ],
+                    'numeric_col' => [
+                        'type' => 'decimal',
+                        'dbType' => 'NUMBER',
+                        'phpType' => 'string',
+                        'allowNull' => true,
+                        'autoIncrement' => false,
+                        'enumValues' => null,
+                        'size' => 22,
+                        'precision' => 5,
+                        'scale' => 2,
+                        'defaultValue' => '33.22',
+                    ],
+                    'time' => [
+                        'type' => 'timestamp',
+                        'dbType' => 'TIMESTAMP(6)',
+                        'phpType' => 'string',
+                        'allowNull' => false,
+                        'autoIncrement' => false,
+                        'enumValues' => null,
+                        'size' => 11,
+                        'precision' => null,
+                        'scale' => 6,
+                        'defaultValue' => null,
+                    ],
+                    'bool_col' => [
+                        'type' => 'string',
+                        'dbType' => 'CHAR',
+                        'phpType' => 'string',
+                        'allowNull' => false,
+                        'autoIncrement' => false,
+                        'enumValues' => null,
+                        'size' => 1,
+                        'precision' => null,
+                        'scale' => null,
+                        'defaultValue' => null,
+                    ],
+                    'bool_col2' => [
+                        'type' => 'string',
+                        'dbType' => 'CHAR',
+                        'phpType' => 'string',
+                        'allowNull' => true,
+                        'autoIncrement' => false,
+                        'enumValues' => null,
+                        'size' => 1,
+                        'precision' => null,
+                        'scale' => null,
+                        'defaultValue' => '1',
+                    ],
+                    'ts_default' => [
+                        'type' => 'timestamp',
+                        'dbType' => 'TIMESTAMP(6)',
+                        'phpType' => 'string',
+                        'allowNull' => false,
+                        'autoIncrement' => false,
+                        'enumValues' => null,
+                        'size' => 11,
+                        'precision' => null,
+                        'scale' => 6,
+                        'defaultValue' => null,
+                    ],
+                    'bit_col' => [
+                        'type' => 'string',
+                        'dbType' => 'CHAR',
+                        'phpType' => 'string',
+                        'allowNull' => false,
+                        'autoIncrement' => false,
+                        'enumValues' => null,
+                        'size' => 3,
+                        'precision' => null,
+                        'scale' => null,
+                        'defaultValue' => '130', // b'10000010'
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    public function columnsTypeChar(): array
+    {
+        return [
+            ['char_col', 'string', 100, 'CHAR'],
+            ['char_col2', 'string', 100, 'VARCHAR2'],
+            ['char_col3', 'string', 4000, 'VARCHAR2'],
+        ];
+    }
+
+    public function constraints(): array
+    {
+        $constraints = parent::constraints();
+
+        $constraints['1: check'][2][0]->expression('"C_check" <> \'\'');
+        $constraints['1: check'][2][] = (new CheckConstraint())
+            ->name(AnyValue::getInstance())
+            ->columnNames(['C_id'])
+            ->expression('"C_id" IS NOT NULL');
+        $constraints['1: check'][2][] = (new CheckConstraint())
+            ->name(AnyValue::getInstance())
+            ->columnNames(['C_not_null'])
+            ->expression('"C_not_null" IS NOT NULL');
+        $constraints['1: check'][2][] = (new CheckConstraint())
+            ->name(AnyValue::getInstance())
+            ->columnNames(['C_unique'])
+            ->expression('"C_unique" IS NOT NULL');
+        $constraints['1: check'][2][] = (new CheckConstraint())
+            ->name(AnyValue::getInstance())
+            ->columnNames(['C_default'])
+            ->expression('"C_default" IS NOT NULL');
+
+        $constraints['2: check'][2][] = (new CheckConstraint())
+            ->name(AnyValue::getInstance())
+            ->columnNames(['C_id_1'])
+            ->expression('"C_id_1" IS NOT NULL');
+        $constraints['2: check'][2][] = (new CheckConstraint())
+            ->name(AnyValue::getInstance())
+            ->columnNames(['C_id_2'])
+            ->expression('"C_id_2" IS NOT NULL');
+
+        $constraints['3: foreign key'][2][0]->foreignSchemaName('SYSTEM');
+        $constraints['3: foreign key'][2][0]->onUpdate(null);
+        $constraints['3: index'][2] = [];
+        $constraints['3: check'][2][] = (new CheckConstraint())
+            ->name(AnyValue::getInstance())
+            ->columnNames(['C_fk_id_1'])
+            ->expression('"C_fk_id_1" IS NOT NULL');
+        $constraints['3: check'][2][] = (new CheckConstraint())
+            ->name(AnyValue::getInstance())
+            ->columnNames(['C_fk_id_2'])
+            ->expression('"C_fk_id_2" IS NOT NULL');
+        $constraints['3: check'][2][] = (new CheckConstraint())
+            ->name(AnyValue::getInstance())
+            ->columnNames(['C_id'])
+            ->expression('"C_id" IS NOT NULL');
+
+        $constraints['4: check'][2][] = (new CheckConstraint())
+            ->name(AnyValue::getInstance())
+            ->columnNames(['C_id'])
+            ->expression('"C_id" IS NOT NULL');
+        $constraints['4: check'][2][] = (new CheckConstraint())
+            ->name(AnyValue::getInstance())
+            ->columnNames(['C_col_2'])
+            ->expression('"C_col_2" IS NOT NULL');
+
+        return $constraints;
+    }
+
+    public function tableSchemaWithDbSchemes(): array
+    {
+        return [
+            ['animal', 'animal', 'dbo'],
+            ['dbo.animal', 'animal', 'dbo'],
+            ['"dbo"."animal"', 'animal', 'dbo'],
+            ['"other"."animal2"', 'animal2', 'other',],
+            ['other."animal2"', 'animal2', 'other',],
+            ['other.animal2', 'animal2', 'other',],
+            ['catalog.other.animal2', 'animal2', 'other'],
+        ];
+    }
+}
