1495565780e145044dee8381d02c8cbc1d48eab6
Wilmer Arambula <terabytesoftw@gmail.com>
2022-12-28 16:01:20 -0300

    Add more tests.

--- tests/QueryTest.php
+++ tests/QueryTest.php
@@ -5,14 +5,14 @@ declare(strict_types=1);
 namespace Yiisoft\Db\Oracle\Tests;
 
 use Yiisoft\Db\Oracle\Tests\Support\TestTrait;
-use Yiisoft\Db\Tests\AbstractQueryTest;
+use Yiisoft\Db\Tests\Common\CommonQueryTest;
 
 /**
  * @group oracle
  *
  * @psalm-suppress PropertyNotSetInConstructor
  */
-final class QueryTest extends AbstractQueryTest
+final class QueryTest extends CommonQueryTest
 {
     use TestTrait;
 }
