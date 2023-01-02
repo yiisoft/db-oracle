f27f01c2df6733eaef57079e39b5d33029099c58
Wilmer Arambula <terabytesoftw@gmail.com>
2023-01-02 09:35:52 -0300

    add `testAmbiguousColumnIndexBy()`.

--- tests/QueryTest.php
+++ tests/QueryTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Yiisoft\Db\Oracle\Tests;
 
 use Yiisoft\Db\Oracle\Tests\Support\TestTrait;
+use Yiisoft\Db\Query\Query;
 use Yiisoft\Db\Tests\Common\CommonQueryTest;
 
 /**
@@ -15,4 +16,25 @@ use Yiisoft\Db\Tests\Common\CommonQueryTest;
 final class QueryTest extends CommonQueryTest
 {
     use TestTrait;
+
+    /**
+     * Ensure no ambiguous column error occurs on indexBy with JOIN.
+     *
+     * @link https://github.com/yiisoft/yii2/issues/13859
+     */
+    public function testAmbiguousColumnIndexBy(): void
+    {
+        $db = $this->getConnection(true);
+
+        $selectExpression = "[[customer]].[[name]] || ' in ' || [[p]].[[description]] name";
+
+        $result = (new Query($db))
+            ->select([$selectExpression])
+            ->from('customer')
+            ->innerJoin('profile p', '[[customer]].[[profile_id]] = [[p]].[[id]]')
+            ->indexBy('id')
+            ->column();
+
+        $this->assertSame([1 => 'user1 in profile customer 1', 3 => 'user3 in profile customer 3'], $result);
+    }
 }
