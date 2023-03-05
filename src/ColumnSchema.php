156a5ba4fb5ab545aeaf16e29165b99a2986789f
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2023-03-05 06:54:06 -0300

    Add phpdoc `ColumnSchema`. (#187)

--- src/ColumnSchema.php
+++ src/ColumnSchema.php
@@ -14,7 +14,27 @@ use function preg_replace;
 use function uniqid;
 
 /**
- * Class ColumnSchema for Oracle database
+ * Represents the metadata of a column in a database table for Oracle Server. It provides information about the column's
+ * name, type, size, precision, and other details.
+ *
+ * Is used to store and retrieve metadata about a column in a database table. It is typically used in conjunction with
+ * the TableSchema class, which represents the metadata of a database table as a whole.
+ *
+ * Here is an example of how the ColumnSchema class might be used:
+ *
+ * ```php
+ * use Yiisoft\Db\Mysql\ColumnSchema;
+ *
+ * $column = new ColumnSchema();
+ * $column->name('id');
+ * $column->allowNull(false);
+ * $column->dbType('number');
+ * $column->phpType('integer');
+ * $column->type('integer');
+ * $column->defaultValue(0);
+ * $column->autoIncrement(true);
+ * $column->primaryKey(true);
+ * ```
  */
 final class ColumnSchema extends AbstractColumnSchema
 {
