b07035b0154d7cb9f159f9cf28b1bd5b303cf324
Sergei Predvoditelev <sergei@predvoditelev.ru>
2023-05-08 14:16:33 +0300

    Enable rector CI

--- rector.php
+++ rector.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 
 use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
+use Rector\Php71\Rector\FuncCall\CountOnNullRector;
+use Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
 use Rector\Set\ValueObject\LevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->disableParallel();
-
     $rectorConfig->paths([
         __DIR__ . '/src',
         __DIR__ . '/tests',
@@ -20,5 +20,12 @@ return static function (RectorConfig $rectorConfig): void {
     // define sets of rules
     $rectorConfig->sets([
         LevelSetList::UP_TO_PHP_80,
+    ]);
+
+    $rectorConfig->skip([
+        ClosureToArrowFunctionRector::class,
+        CountOnNullRector::class => [
+            'src/Command.php',
+        ],
     ]);
 };
