79ab45cdc81cf2cde39369575aaa1d47cc5af2b2
Tigrov <rrr-r@ya.ru>
2023-09-01 18:50:14 +0700

    Refactor with inheritance

--- src/BinaryColumnSchema.php
+++ src/BinaryColumnSchema.php
@@ -4,29 +4,14 @@ declare(strict_types=1);
 
 namespace Yiisoft\Db\Oracle;
 
-use PDO;
-use Yiisoft\Db\Command\Param;
 use Yiisoft\Db\Command\ParamInterface;
 use Yiisoft\Db\Expression\Expression;
-use Yiisoft\Db\Expression\ExpressionInterface;
-use Yiisoft\Db\Helper\DbStringHelper;
-use Yiisoft\Db\Schema\Column\AbstractColumnSchema;
-use Yiisoft\Db\Schema\SchemaInterface;
+use Yiisoft\Db\Schema\Column\BinaryColumnSchema as BaseBinaryColumnSchema;
 
-use function is_float;
-use function is_resource;
 use function is_string;
 
-final class BinaryColumnSchema extends AbstractColumnSchema
+final class BinaryColumnSchema extends BaseBinaryColumnSchema
 {
-    public function __construct(string $name)
-    {
-        parent::__construct($name);
-
-        $this->type(SchemaInterface::TYPE_BINARY);
-        $this->phpType(SchemaInterface::PHP_TYPE_RESOURCE);
-    }
-
     public function dbTypecast(mixed $value): mixed
     {
         if ($this->getDbType() === 'BLOB') {
@@ -41,13 +26,6 @@ final class BinaryColumnSchema extends AbstractColumnSchema
             }
         }
 
-        return match (true) {
-            is_string($value) => new Param($value, PDO::PARAM_LOB),
-            $value === null, is_resource($value), $value instanceof ExpressionInterface => $value,
-            /** ensure type cast always has . as decimal separator in all locales */
-            is_float($value) => DbStringHelper::normalizeFloat($value),
-            $value === false => '0',
-            default => (string) $value,
-        };
+        return parent::dbTypecast($value);
     }
 }
