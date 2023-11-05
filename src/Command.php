04a4afa68e45ed0e01f750c3a69073cfabfc83c3
Tigrov <rrr-r@ya.ru>
2023-11-05 13:35:53 +0700

    Move methods from `Command` to `AbstractPdoCommand` class

--- src/Command.php
+++ src/Command.php
@@ -91,11 +91,6 @@ final class Command extends AbstractPdoCommand
         return $this->setSql($sql)->queryColumn();
     }
 
-    protected function getQueryBuilder(): QueryBuilderInterface
-    {
-        return $this->db->getQueryBuilder();
-    }
-
     protected function bindPendingParams(): void
     {
         $paramsPassedByReference = [];
@@ -114,41 +109,6 @@ final class Command extends AbstractPdoCommand
                 );
             } else {
                 $this->pdoStatement?->bindValue($name, $value->getValue(), $value->getType());
-            }
-        }
-    }
-
-    /**
-     * @psalm-suppress UnusedClosureParam
-     *
-     * @throws Throwable
-     */
-    protected function internalExecute(?string $rawSql): void
-    {
-        $attempt = 0;
-
-        while (true) {
-            try {
-                if (
-                    ++$attempt === 1
-                    && $this->isolationLevel !== null
-                    && $this->db->getTransaction() === null
-                ) {
-                    $this->db->transaction(
-                        fn () => $this->internalExecute($rawSql),
-                        $this->isolationLevel
-                    );
-                } else {
-                    $this->pdoStatement?->execute();
-                }
-                break;
-            } catch (PDOException $e) {
-                $rawSql = $rawSql ?: $this->getRawSql();
-                $e = (new ConvertException($e, $rawSql))->run();
-
-                if ($this->retryHandler === null || !($this->retryHandler)($e, $attempt)) {
-                    throw $e;
-                }
             }
         }
     }
