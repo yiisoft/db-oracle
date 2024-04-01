1ec1e063fbea2158bc827f7251cdfa7c1c188877
Tigrov <rrr-r@ya.ru>
2024-04-01 19:22:21 +0700

    Update according main PR

--- src/Builder/ExpressionBuilder.php
+++ src/Builder/ExpressionBuilder.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace Yiisoft\Db\Oracle\Builder;
 
-use Yiisoft\Db\Expression\ExpressionBuilder as BaseExpressionBuilder;
+use Yiisoft\Db\Expression\AbstractExpressionBuilder;
 use Yiisoft\Db\Oracle\SqlParser;
 
-final class ExpressionBuilder extends BaseExpressionBuilder
+final class ExpressionBuilder extends AbstractExpressionBuilder
 {
     protected function createSqlParser(string $sql): SqlParser
     {
