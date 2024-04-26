aa34bf51338a1a219ed905b63b8673ebfab858ba
Arhell <arhell333@gmail.com>
2024-04-27 00:20:37 +0300

    update src folder link

--- src/Dsn.php
+++ src/Dsn.php
@@ -29,7 +29,7 @@ final class Dsn extends AbstractDsn
     /**
      * @return string The Data Source Name, or DSN, contains the information required to connect to the database.
      *
-     * Please refer to the [PHP manual](http://php.net/manual/en/pdo.construct.php) on the format of the DSN string.
+     * Please refer to the [PHP manual](https://php.net/manual/en/pdo.construct.php) on the format of the DSN string.
      *
      * The `driver` array key is used as the driver prefix of the DSN, all further key-value pairs are rendered as
      * `key=value` and concatenated by `;`. For example:
