7785feb42204ffdf56e046db9fd1656451058072
Tigrov <rrr-r@ya.ru>
2025-02-22 15:27:18 +0700

    Revert env

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
-if (getenv('ENVIRONMENT', true) === 'local') {
-    putenv('YII_ORACLE_HOST=oracle');
+if (getenv('ENVIRONMENT', local_only: true) === 'local') {
+    $dotenv = Dotenv\Dotenv::createUnsafeImmutable(__DIR__);
+    $dotenv->load();
 }
