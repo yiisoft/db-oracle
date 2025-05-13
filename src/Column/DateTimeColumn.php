7ea056602128ce6df1e6d140ef0895f0e71eeee4
Tigrov <rrr-r@ya.ru>
2025-05-13 17:11:54 +0700

    Add line to CHANGELOG.md and docs [skip ci]

--- src/Column/DateTimeColumn.php
+++ src/Column/DateTimeColumn.php
@@ -13,6 +13,22 @@ use function explode;
 use function is_string;
 use function str_replace;
 
+/**
+ * Represents the metadata for a datetime column.
+ *
+ * > [!WARNING]
+ * > Oracle DBMS converts `TIMESTAMP WITH LOCAL TIME ZONE` column type values from database session time zone
+ * > to the database time zone for storage, and back from the database time zone to the session time zone when retrieve
+ * > the values.
+ *
+ * `TIMESTAMP WITH LOCAL TIME ZONE` database type does not store time zone offset and require to convert datetime values
+ * to the database session time zone before insert and back to the PHP time zone after retrieve the values.
+ * This will be done in the {@see dbTypecast()} and {@see phpTypecast()} methods and guarantees that the values
+ * are stored in the database in the correct time zone.
+ *
+ * To avoid possible time zone issues with the datetime values conversion, it is recommended to set the PHP and database
+ * time zones to UTC.
+ */
 final class DateTimeColumn extends \Yiisoft\Db\Schema\Column\DateTimeColumn
 {
     public function dbTypecast(mixed $value): string|ExpressionInterface|null
