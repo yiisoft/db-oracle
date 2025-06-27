7731d6bc940c18d223188e0241302c39402f24d3
Sergei Tigrov <rrr-r@ya.ru>
2025-06-27 15:15:57 +0700

    Refactor workflows (#335)

--- rector.php
+++ rector.php
@@ -8,29 +8,19 @@ use Rector\DeadCode\Rector\StaticCall\RemoveParentCallWithoutParentRector;
 use Rector\Php73\Rector\String_\SensitiveHereNowDocRector;
 use Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector;
 use Rector\Php81\Rector\Property\ReadOnlyPropertyRector;
-use Rector\Set\ValueObject\LevelSetList;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([
+return RectorConfig::configure()
+    ->withPaths([
         __DIR__ . '/src',
-        /**
-         * Disabled ./tests directory due to different branches with main package when testing
-         */
-        // __DIR__ . '/tests',
-    ]);
-
-    // register a single rule
-    $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
-
-    // define sets of rules
-    $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_81,
-    ]);
-
-    $rectorConfig->skip([
+        __DIR__ . '/tests',
+    ])
+    ->withPhpSets(php81: true)
+    ->withRules([
+        InlineConstructorDefaultToPropertyRector::class,
+    ])
+    ->withSkip([
         NullToStrictStringFuncCallArgRector::class,
         ReadOnlyPropertyRector::class,
         SensitiveHereNowDocRector::class,
         RemoveParentCallWithoutParentRector::class,
     ]);
-};
