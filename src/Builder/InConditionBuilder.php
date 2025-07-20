0ab20685ce943e83d5361f9c1f5b63b3e401bbf9
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-07-20 11:29:24 +0300

    Adapt to specific condition interfaces removing in `yiisoft/db` package (#341)

--- src/Builder/InConditionBuilder.php
+++ src/Builder/InConditionBuilder.php
@@ -9,7 +9,7 @@ use InvalidArgumentException;
 use Yiisoft\Db\Exception\InvalidConfigException;
 use Yiisoft\Db\Exception\NotSupportedException;
 use Yiisoft\Db\Expression\ExpressionInterface;
-use Yiisoft\Db\QueryBuilder\Condition\Interface\InConditionInterface;
+use Yiisoft\Db\QueryBuilder\Condition\InCondition;
 
 use function array_slice;
 use function array_unshift;
@@ -17,14 +17,14 @@ use function count;
 use function is_array;
 
 /**
- * Build an object of {@see \Yiisoft\Db\QueryBuilder\Condition\InCondition} into SQL expressions for Oracle Server.
+ * Build an object of {@see InCondition} into SQL expressions for Oracle Server.
  */
 final class InConditionBuilder extends \Yiisoft\Db\QueryBuilder\Condition\Builder\InConditionBuilder
 {
     /**
      * The Method builds the raw SQL from the $expression that won't be additionally escaped or quoted.
      *
-     * @param InConditionInterface $expression The expression to build.
+     * @param InCondition $expression The expression to build.
      * @param array $params The binding parameters.
      *
      * @throws Exception
@@ -55,7 +55,7 @@ final class InConditionBuilder extends \Yiisoft\Db\QueryBuilder\Condition\Builde
      *
      * @return string|null `null` when split isn't required. Otherwise - built SQL condition.
      */
-    protected function splitCondition(InConditionInterface $condition, array &$params): string|null
+    protected function splitCondition(InCondition $condition, array &$params): string|null
     {
         $operator = $condition->getOperator();
         $values = $condition->getValues();
