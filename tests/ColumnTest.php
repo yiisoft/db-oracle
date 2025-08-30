fd28f74224c02c8c9ce9e9e49f131d94c625ba85
Tigrov <rrr-r@ya.ru>
2025-08-30 12:47:47 +0700

    Rearrange expression namespaces

--- tests/ColumnTest.php
+++ tests/ColumnTest.php
@@ -10,7 +10,7 @@ use PDO;
 use PHPUnit\Framework\Attributes\DataProviderExternal;
 use Yiisoft\Db\Connection\ConnectionInterface;
 use Yiisoft\Db\Expression\Expression;
-use Yiisoft\Db\Expression\Param;
+use Yiisoft\Db\Expression\Value\Param;
 use Yiisoft\Db\Oracle\Column\BinaryColumn;
 use Yiisoft\Db\Oracle\Column\ColumnBuilder;
 use Yiisoft\Db\Oracle\Column\JsonColumn;
