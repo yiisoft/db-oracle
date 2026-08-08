515b49a9dc63a45c59db70196a69ee9427af0713
Sergei Predvoditelev <sergei@predvoditelev.ru>
2026-08-08 08:20:39 +0300

    Add Composer Dependency Analyser

---
+++ composer-dependency-analyser.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
+use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
+
+return (new Configuration())
+    ->disableComposerAutoloadPathScan()
+    ->setFileExtensions(['php'])
+    ->addPathToScan(__DIR__ . '/src', isDev: false)
+    ->addPathToScan(__DIR__ . '/tests', isDev: true)
+    ->ignoreErrorsOnExtension('ext-pdo_oci', [ErrorType::UNUSED_DEPENDENCY]);
